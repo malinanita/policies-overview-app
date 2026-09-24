d912666625aed7709e724e0c5075ef15646ec108
Malin Eek <malin.eek@hotmail.com>
2026-09-24 17:52:42 +0200

    feat: fetch policies from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,35 @@
 import './App.css'
+import { useEffect, useState } from 'react'
+import type { Policy } from './types/policy.ts'
 
 function App() {
+  const [policies, setPolicies] = useState<Policy[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error,setError] = useState<string | null>(null);
+  const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
+  useEffect(() => {
+    //Hämta policies
+    const fetchPolicies = async () => {
+      try {
+        const response = await fetch(`${apiBaseUrl}/policies/List`);
+        
+        if (!response.ok) {
+          throw new Error(`Failed to fetch policies: ${response.status}`);
+        }
+
+        const policiesData: Policy[] = await response.json();
+        //console.log(policiesData);
+        setPolicies(policiesData);
+      } catch {
+        setError('Något gick fel vid hämtning av policies');
+      } finally {
+        setIsLoading(false);
+      }
+
+    }
+    fetchPolicies();
+  },[]);
 
   return (
     <>
